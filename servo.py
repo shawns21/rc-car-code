a71cc7ff5b290a9ce742fa3df5792c0684fee0f6
shawns21 <shawnsinanan21@gmail.com>
2022-04-18 15:47:17 -0400

    Create servo.py

---
+++ servo.py
@@ -0,0 +1,22 @@
+from gpiozero import Servo
+from time import sleep
+import math
+import curses
+
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory()
+
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
+servo = Servo(17, min_pulse_width = 0.5 / 1000, max_pulse_width = 2.5 / 1000, pin_factory = factory)
+
+while True:
+    char = screen.getch()
+    if char == curses.KEY_RIGHT:
+        servo.value = -1
+    elif char == curses.KEY_LEFT:
+        servo.value = 1
